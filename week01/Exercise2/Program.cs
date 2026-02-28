51273d1dededc3b5c31bc5362ac4a76eb6c3ff9e
kingsley-byu <osaruyiosas@gmail.com>
2026-02-28 13:48:03 +0100

    worked on exercise 1 and 2 of week 1

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,82 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+      Console.Write("what is the student's grade? ");
+        string grade = Console.ReadLine();
+        int studentGrade = int.Parse(grade);
+        string letter = "";
+        string sign = "";
+        if (studentGrade >= 90)
+        {
+            letter = "A";
+            if (studentGrade % 10 >= 7)
+            {
+                
+            }
+            else if (studentGrade % 10 < 3)
+            {
+               sign = "-"; 
+            }
+
+        }
+        else if (studentGrade >=80)
+        {
+            letter = "B";
+            if (studentGrade % 10 >= 7)
+            {
+                sign = "+";
+            }
+            else if (studentGrade % 10 <3)
+            {
+                sign = "-";
+            }
+        }
+        else if (studentGrade >=70)
+        {
+            letter = "C";
+            if (studentGrade % 10 >= 7)
+            {
+                sign = "+";
+            }
+            else if (studentGrade % 10 <3)
+            {
+                sign = "-";
+            }
+        }
+        else if (studentGrade >=60)
+        {
+            letter = "D";
+            if (studentGrade % 10 >= 7)
+            {
+                sign = "+";
+            }
+            else if (studentGrade % 10 <3)
+            {
+                sign = "-";
+            }
+        }
+        else 
+        {
+            letter = "F";
+            sign = "";
+        }
+
+        if (sign != "")
+        {
+            Console.WriteLine($"Your grade is: {letter}{sign}");
+        }
+        else
+        {
+            Console.WriteLine($"Your grade is {letter}");
+        }
+
+        if (studentGrade >= 70)
+        {
+            Console.WriteLine("Congratulation you passed the course");
+        }
+        else
+        {
+            Console.WriteLine("Sorry you did not pass, work harder next time.");
+        } 
     }
 }
