74817997a5d31109cafde1236bdbf16711a6e5c6
kingsley-byu <osaruyiosas@gmail.com>
2026-03-27 14:14:07 +0100

    added comment to the video object created

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -4,10 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        private List<Video> _video = new List<Video>();
+        public List<Video> _video = new List<Video>();
 
         Video video1 = new Video("Best of  lionel messi", "Prince bolton", 689);
         Comment comment1 = new Comment("osawaru kingsley", "wow what a performance by lionel messi");
+        Comment comment2 = new Comment("prince wells", "magical messi");
+        Comment comment3 = new Comment("Adam steve", "Ohh My world!!, magical messsi");
+        Comment comment4 = new Comment("clark newton", "beaitiful by lionel messi");
+        video1.AddComment(comment1);
+        video1.AddComment(comment2);
+        video1.AddComment(comment3);
+        video1.AddComment(comment4);
+
+
         
 
         
