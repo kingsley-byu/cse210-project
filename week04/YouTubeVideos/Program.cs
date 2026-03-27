40847d238ae3dba77757e457e5419339d5844573
kingsley-byu <osaruyiosas@gmail.com>
2026-03-27 15:18:15 +0100

    made correction to the program class by adding the each video to the list if videos

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -17,24 +17,26 @@ class Program
         video1.AddComment(comment2);
         video1.AddComment(comment3);
         video1.AddComment(comment4);
+        videos.Add(video1);
 
         Video video2 = new Video("lion and the snake", "james clark", 567);
         Comment comment5 = new Comment("kate","animal survival");
         Comment comment6 = new Comment("Esther", "flawless win");
         Comment comment7 = new Comment("Andrew Wilson", "animal chaos");
-        Comment comment8 = new Comment("Keneth", "Nice fight");
+        Comment comment8 = new Comment("Kenneth", "Nice fight");
 
             
         video2.AddComment(comment5);
         video2.AddComment(comment6);
         video2.AddComment(comment7);
         video2.AddComment(comment8);
+        videos.Add(video2);
 
 
-        Video video3 = new Video("Temple Beatify", "Adam Kane", 678 );
+        Video video3 = new Video("Temple Beatify", "Adam Kane", 678);
         Comment comment9 = new Comment("Edward prince", "house of the lord");
         Comment comment10 = new Comment("Edith praise", "beautiful is the house of the lord");
-        Comment comment11= new Comment("kingsley osas", "Ilove the temple");
+        Comment comment11= new Comment("kingsley osas", "I love the temple");
         Comment comment12= new Comment("Akon smith", "Look forward to visit the temple");
 
             
@@ -42,10 +44,11 @@ class Program
         video3.AddComment(comment10);
         video3.AddComment(comment11);
         video3.AddComment(comment12);
+        videos.Add(video3);
 
 
         Video video4 = new Video("The voice of the prophet","Agon walter", 890);
-        Comment comment13 = new Comment("kingston", "God bless our Prophect");
+        Comment comment13 = new Comment("kingston", "God bless our Prophet");
         Comment comment14= new Comment("Philip", "I sustain the prophet");
         Comment comment15= new Comment("John Lamn", "Thank God for a prophet voice");
         Comment comment16= new Comment("Aston villa", "God is love");
@@ -56,6 +59,7 @@ class Program
         video4.AddComment(comment14);
         video4.AddComment(comment15);
         video4.AddComment(comment16);
+        videos.Add(video4);
 
         foreach (Video video in videos)
         {
